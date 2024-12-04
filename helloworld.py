21b62bffadbc55605d54a6ec95abd736e22f541f
Arctic-Bird <colmull64@gmail.com>
2024-12-03 20:44:29 -0500

    Added comments and a little input validation function to helloworld.py. I promise it won't be used as the central hub for this little repository.

--- helloworld.py
+++ helloworld.py
@@ -2,13 +2,13 @@ print("Hello world!")
 
 def multiply():
     while True:
-        try:
+        try: # You can have some input validation in your 'Hello World' program, as a treat.
             input1 = int(input("Enter an integer: "))
             input2 = int(input("Enter ANOTHER integer: "))
             product = input1 * input2
             print(product)
         except ValueError:
-            print("Hey DUMBASS! I asked you to enter INTEGERS!")
+            print("Hey DUMBASS! I asked you to enter INTEGERS!") # It loops until you enter an integer :) Someone buffer overflow this mf
             continue
         else:
             break
