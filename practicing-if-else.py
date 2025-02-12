ac5e5e49059ce751ddc88799db79b9dc44e1b457
Arctic-Bird <colmull64@gmail.com>
2025-02-12 14:32:35 -0500

    Added single line if-else statement to practicing-if-else.py

--- practicing-if-else.py
+++ practicing-if-else.py
@@ -2,11 +2,11 @@ def fizzBuzz():
     for integer in range(1, 101):
         if integer % 15 == 0:
             print("FizzBuzz")
-        elif integer % 5 == 0:
+        elif integer % 5 == 0: # Elif must always come after an if statement!
             print("Buzz")
         elif integer % 3 == 0:
             print("Fizz")
-        else:
+        else: # The else statement after if and elif statements is optional
             print(integer)
         
     return integer
@@ -17,3 +17,7 @@ print(fizzBuzz())
 then it'll apply to the integer % 15 == 0 statement as well. So, check if integer % 15 is 0 FIRST!
 After that, the elif statements can be in either order. I just did it descending because it looks nicer.
 '''
+
+# Single line if statement:
+num = 6
+fizz = 'Fizz' if num % 3 == 0 else n # This is similar to list and dictionary comprehensions!
